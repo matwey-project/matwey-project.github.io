73ecedaef17aa706449ebb619c56c6826b4d2b4a
matwey-project <xzzmanzzx@gmail.com>
2020-06-01 16:36:28 +0300

    Gazele-Serpuhov
    Gazele-Serpuhov

---
+++ 049_Gazele-Serpuhov/js/map-option.js
@@ -0,0 +1,220 @@
+var options = {
+      styles: [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+    };
+
+// var markers = [
+//   {
+//     coordinates: {lat: 51.491686, lng: -0.038784},
+//     image: '/img/marker.svg',
+//     info: '<p>text</p>'
+//   },
+// ];
+
+var coords = [
+    {lat: 50.0274617, lng: 36.2225905, zoom: 12},
+    {lat: 51.5177099, lng: 0.1254583, zoom: 12},
+    {lat: 1.2890698, lng: 103.8447739, zoom: 12}
+];
+var markers = [];
+var maps = [];
+
+var icon = {
+    url: "../img/icon/map-marker.svg"
+};
+
+function initMap() {
+    for(var i = 0, length = coords.length; i < length; i++)
+    {
+        var point = coords[i];
+        var latlng = new google.maps.LatLng(point.lat, point.lng);
+
+        /**********Div's are called in this line*********/
+        maps[i] = new google.maps.Map(document.getElementsByClassName('map')[i], {
+            zoom: point.zoom,
+            center: latlng,
+            options
+        });
+
+        markers[i] = new google.maps.Marker({
+            position: latlng,
+            map: maps[i],
+            icon: icon
+        });
+    }
+}
+google.maps.event.addDomListener(window, "load", initMap);
